307a465bd952ee741ae737b696236399f2c90f2f
Ansah-98 <yungtupac99@gmail.com>
2023-07-14 09:18:36 -0700

    updates

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the program
+ *
+ *
+ *
+ *Return: Always return 0(success)
+ *
+ */
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
