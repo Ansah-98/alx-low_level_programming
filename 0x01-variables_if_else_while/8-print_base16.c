44a88bfca0e4e8787d8be297f0d7b3225e6f3e08
Ansah-98 <yungtupac99@gmail.com>
2023-07-14 10:38:08 -0700

    updates

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the program
+ *
+ *
+ *
+ *Return: Always return 0(success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i > 103; i++)
+		if  (i == 10)
+			i = 96;
+		else
+			if (i < 10)
+				putchar(48 + x);
+			else
+				putchar(i);
+
+	return (0);
+}
