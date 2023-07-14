3274f478c642f2480c10affb2610878cd83883f7
Ansah-98 <yungtupac99@gmail.com>
2023-07-13 18:49:53 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,14 @@
 /**
 *main - Entry point of the program
 *
-* Description: Prints the message "with proper grammar, but the outcome is a piece of art,"
+* Description: Prints the message 
+*"with proper grammar, but the outcome is a piece of art,"
 *followed by a new line.
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("%s\n", "with proper grammar, but the outcome is a piece of art");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
