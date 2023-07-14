5e9ca8fc1228a7d1265b6be9643677eeb694c794
Ansah-98 <yungtupac99@gmail.com>
2023-07-14 10:07:52 -0700

    updates

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the program
+ *
+ *
+ *
+ *Return: Always return 0(success)
+ *
+ */
+int main(void)
+{
+	int i;
+	int j = 123;
+
+	for (i = 97 ; i <= j ; i++)
+	{
+		if ((i != j) && (i != 101 || i != 113))
+			putchar(i);
+		else if (i == j)
+			putchar(10);
+		else
+			continue;
+	}
+	return (0);
+}
