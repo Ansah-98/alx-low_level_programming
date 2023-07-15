a4ed82a53a9d0af0bf46b68c4bb733e11579dd0e
Ansah-98 <yungtupac99@gmail.com>
2023-07-15 06:45:35 -0700

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the program
+ *
+ *
+ *
+ *Return: Always return 0(success)
+ *
+ */
+int main(void)
+{
+
+	int i;
+	int j;
+
+	i = 48;
+	j = 49;
+
+	while (i < 56)
+	{
+		for (j; j < 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar(10);
+
+	return (0);
+}
