9926f46cb7e0d13bb2202a646dbf054b252e783e
Ansah-98 <yungtupac99@gmail.com>
2023-07-15 06:55:44 -0700

    updates

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -23,8 +23,11 @@ int main(void)
 		{
 			putchar(i);
 			putchar(j);
-			putchar(',');
-			putchar(' ');
+			if (i != 56)
+			{ 
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		i++;
 		j = i + 1;
