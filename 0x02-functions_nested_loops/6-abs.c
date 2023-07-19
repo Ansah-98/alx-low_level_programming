a3171bf1317cd5db8226cb556da9965a323c44fa
Ansah-98 <yungtupac99@gmail.com>
2023-07-19 03:57:14 -0700

    updates

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - prints lowercase and new line
+ *
+ * @n: takes an integer value or a character value
+ *
+ * Return: return 0 when is not lower case return 1 when lower case
+ *
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
