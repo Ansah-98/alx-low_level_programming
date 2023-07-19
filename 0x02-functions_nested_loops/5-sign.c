14fa25d546e2beed68ecbbd40920f1615c7fbae9
Ansah-98 <yungtupac99@gmail.com>
2023-07-19 03:46:36 -0700

    updates

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - prints lowercase and new line
+ *
+ * @n: takes an integer value or a character value
+ *
+ * Return: return 0 when is not lower case return 1 when lower case
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43)
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48)
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1)
+	}
+}
