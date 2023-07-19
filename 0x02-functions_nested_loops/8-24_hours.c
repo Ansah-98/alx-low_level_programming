8a56a569ff82b81c04c75b0df64194d55cf5bbee
Ansah-98 <yungtupac99@gmail.com>
2023-07-19 08:13:59 -0700

    updates

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints lowercase and new line
+ *
+ *
+ * Return: return 0 when is not lower case return 1 when lower case
+ *
+ */
+
+void jack_bauer(void)
+{
+	int min;
+	int hour;
+
+	for (hour = 0; hour < 24; hour++ )
+	{
+		for (min = 0; minute < 60; minute++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0'+ (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n')
+		}
+	}
+}
