189451747db8e16d2ccd9f8a304cfd5b259cc265
Ansah-98 <yungtupac99@gmail.com>
2023-07-19 08:18:23 -0700

    updates

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,14 +13,14 @@ void jack_bauer(void)
 	int min;
 	int hour;
 
-	for (hour = 0; hour < 24; hour++ )
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (min = 0; min < 60; min++)
 		{
 			_putchar('0' + (hour / 10));
 			_putchar('0' + (hour % 10));
 			_putchar(':');
-			_putchar('0'+ (min / 10));
+			_putchar('0' + (min / 10));
 			_putchar('0' + (min % 10));
 			_putchar('\n');
 		}
