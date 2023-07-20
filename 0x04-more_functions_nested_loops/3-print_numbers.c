a6eafb372e7a73b963035eaf797c0d6fc8b8b188
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 08:41:26 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - return no shit fuck this shit
+ *
+ * @a: receives a character
+ *
+ * @b: receives and integer
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,5 @@
 int _isdigit(int c);
 int _isupper(int c);
 int mul(int a, int b);
+int _putchar(char c);
 #endif /* MAIN_H */
