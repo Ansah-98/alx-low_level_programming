407ac5fbe5950cafc37f63342f5560cb2f3e4a39
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 08:49:01 -0700

    updates

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,26 @@
+/**
+ * main - main entry of the program
+ *
+ *
+ *
+ * Return: always return 0 (success)
+ *
+ */
+
+
+#include "main.h"
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _isdigit(int c);
 int _isupper(int c);
 int mul(int a, int b);
 int _putchar(char c);
+void print_numbers(void);
 #endif /* MAIN_H */
