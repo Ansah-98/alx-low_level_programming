cc1891e09459dc27128aec7c92ac6a28da051abc
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 10:26:39 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+/**
+ * more_numbers - return no shit fuck this shit
+ *
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{	_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+}
