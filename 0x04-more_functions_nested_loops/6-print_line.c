ca510784e821fe37396683e1f3670116a6777786
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 10:50:26 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+/**
+ * more_numbers - return no shit fuck this shit
+ *
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include "main.h"
+
+void print_line(int n)
+{
+	while (n != 0)
+	{
+		_putchar(95);
+		--n;
+	}
+	_putchar('\n');
+}
