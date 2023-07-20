346f97a6ed6de2aa399017d2b714a6150cc1e758
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 09:28:03 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+/**
+ * print_numbers - return no shit fuck this shit
+ *
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include "main.h"
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue
+		else
+			_putchar(i + '0');
+	}
+}
