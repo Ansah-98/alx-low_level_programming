1ea11113c2127c111e61b6aca8fc13f185ff8a89
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 08:32:11 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+ * mul - return no shit fuck this shit
+ *
+ * @a: receives a character
+ *
+ * @b: receives and integer
+ * 
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b)
+}
