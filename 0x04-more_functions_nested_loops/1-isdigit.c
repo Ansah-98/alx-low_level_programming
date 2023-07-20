9d92809a629869d54fe73c371edfdcee9b5faaac
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 08:23:47 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - return no shit fuck this shit
+ *
+ * @c: receives a character
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 57)
+		return (1);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+int _isdigit(int c);
 int _isupper(int c);
 #endif /* MAIN_H */
