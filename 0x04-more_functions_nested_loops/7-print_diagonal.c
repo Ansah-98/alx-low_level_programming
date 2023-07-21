1496ec06d38a6f8dbd9aa65252dcbd6e8daf740c
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 17:35:59 -0700

    updates

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,11 +20,11 @@ void print_diagonal(int n)
 		{
 			for (space = 0; space < len; space++)
 			{
-				_putchar(" ");
+				_putchar(' ');
 
 			}
-			_putchar('\\')
-			if (len == n - 1)
+			_putchar(92);
+			if (len == (n - 1))
 				continue;
 			_putchar('\n');
 		}
