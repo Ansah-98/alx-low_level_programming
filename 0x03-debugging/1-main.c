0227e3196f3695da02651d9897ef09110b818b9b
Ansah-98 <yungtupac99@gmail.com>
2023-07-20 19:09:03 -0700

    updates

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -17,7 +18,7 @@ int main(void)
 	*	putchar(i)
 	*}
 	*/
-
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
