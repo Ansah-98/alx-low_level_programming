ebe0c79b98767becaee7fd7390b083a27ddd2954
Ansah-98 <yungtupac99@gmail.com>
2023-07-24 05:43:54 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/**
+ * reset_to_98 - return no shit fuck this shit
+ *
+ * @*n: receives a character
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+int reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is some bullsit going in this life and im really loving how this life is panning out well
