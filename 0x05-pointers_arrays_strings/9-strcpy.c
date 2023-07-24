522271f000d52dadb840b9fa2c8c8bfdca6c6514
Ansah-98 <yungtupac99@gmail.com>
2023-07-24 09:20:07 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strcpy - return no shit fuck this shit
+ *
+ * @src: receives a character:
+ *
+ * @dest: dont even about this bullshit
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+char *_strcpy(char *dest, char *src);
+{
+	strcpy(src, dest);
+
+	return (dest);
+}
