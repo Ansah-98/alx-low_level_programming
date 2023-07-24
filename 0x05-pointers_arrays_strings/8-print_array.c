4fff0c014c0fe4aa8ad2943d07103a3f81971690
Ansah-98 <yungtupac99@gmail.com>
2023-07-24 08:22:28 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+i/**
+ * print_array - return no shit fuck this shit
+ *
+ * @a: receives a character:
+ *
+ * @n: dont even about this bullshit
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < strlen(a); i++)
+	{
+		putchar(a[i] + '0');
+	}
+}
