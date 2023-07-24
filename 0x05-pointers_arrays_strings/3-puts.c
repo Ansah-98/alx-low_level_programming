5868c5a401914de9b3ad934233beeadef50d7541
Ansah-98 <yungtupac99@gmail.com>
2023-07-24 06:20:31 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+/**
+ * _puts - return no shit fuck this shit
+ *
+ * @str: receives a character:
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+void _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+}
