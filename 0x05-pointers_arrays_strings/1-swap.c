11a926a3c2182d0d4ba6d521081577a6a0537f8d
Ansah-98 <yungtupac99@gmail.com>
2023-07-24 06:00:54 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+/**
+ * swap_int - return no shit fuck this shit
+ *
+ * @a: receives a character
+ *
+ *
+ * @b
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c  = *a;
+	*a = *b;
+	*b = c;
+}
