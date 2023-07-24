2764b37b5c5ee9313df93fca984e936877967230
Ansah-98 <yungtupac99@gmail.com>
2023-07-24 06:13:17 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - return no shit fuck this shit
+ *
+ * @s: receives a character:
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+#include <string.h>
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
