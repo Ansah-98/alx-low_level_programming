39daabc105c584ee29f7e4138cde82b3c4e40918
Ansah-98 <yungtupac99@gmail.com>
2023-07-24 09:33:18 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+/**
+ * _strcpy - return no shit fuck this shit
+ *
+ * @src: receives a character:
+ *
+ * @dest: dont even about this bullshit
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _atoi(char *s);
+{
+	return atoi(str);int _atoi(char *s);
+}
