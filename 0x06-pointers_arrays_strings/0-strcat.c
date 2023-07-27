1756691ad8a96168e05571c1066c6cddebb4e9c3
Ansah-98 <yungtupac99@gmail.com>
2023-07-27 05:07:03 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+/**
+ * _strcat - return no shit fuck this shit
+ *
+ * @src: receives a character:
+ *
+ * @dest: damn you just disturbing
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest,src));
+}
