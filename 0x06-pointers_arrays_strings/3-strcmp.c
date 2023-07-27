d14d139e491f762acbaf455f8d9270fff1567447
Ansah-98 <yungtupac99@gmail.com>
2023-07-27 06:55:35 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+ * _strncpy - return no shit fuck this shit
+ *
+ * @src: receives a character:
+ *
+ * @dest: damn you just disturbing
+ *
+ * @n : number of times
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
