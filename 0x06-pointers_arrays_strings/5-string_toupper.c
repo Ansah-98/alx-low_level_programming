02c66e50b946db13efab750335e7686b8364e2df
Ansah-98 <yungtupac99@gmail.com>
2023-07-27 08:06:31 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+/**
+ * string_touppper - return no shit fuck this shit
+ *
+ * @str: receives a character:
+ *
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+#include <ctype.h>
+
+char *string_toupper(char *str)
+{
+	for (; *str; *str++)
+	{
+		*str = toupper((unsigned char)*str)
+	}
+
+	return (str);
+}
