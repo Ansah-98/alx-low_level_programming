88fee96f395faf7a74dfaed9a4310bdeafc30633
Ansah-98 <yungtupac99@gmail.com>
2023-07-31 07:01:23 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+/**
+ * _strpbrk - return no shit fuck this shit
+ *
+ * @s: receives a character:
+ *
+ * @accept: damn you just disturbing
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
