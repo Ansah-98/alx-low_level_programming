614ca097304f2eef30d3ac6838c1e914071083c6
Ansah-98 <yungtupac99@gmail.com>
2023-07-31 06:13:55 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr - return no shit fuck this shit
+ *
+ * @s: receives a character:
+ *
+ * @c: damn you just disturbing
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+char *_strchr(char *s, char c)
+{
+	for (; s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (NULL);
+}
