2a3d1b6d731e7df687ceb7b4ffdab91d46539944
Ansah-98 <yungtupac99@gmail.com>
2023-07-31 08:27:08 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+/**
+ * print_diagsums - return no shit fuck this shit
+ *
+ * @a: receives a character:
+ *
+ * @size: this is us
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
