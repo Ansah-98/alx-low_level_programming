f36ed1b701accd444911771c29b86b5e7bacb593
Ansah-98 <yungtupac99@gmail.com>
2023-07-31 06:49:29 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+/**
+ * _strspn - return no shit fuck this shit
+ *
+ * @s: receives a character:
+ *
+ * @accept: damn you just disturbing
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
