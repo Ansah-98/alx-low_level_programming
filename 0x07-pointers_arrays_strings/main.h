6cdac0f224459c43a0d83be3338d44780c62e139
Ansah-98 <yungtupac99@gmail.com>
2023-07-31 05:34:03 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+char *_memset(char *s, char b, unsigned int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+char *_strncat(char *dest, char *_src, int n);
+char* _strncpy(char* dest, const char* src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+#endif /* MAIN_H */
