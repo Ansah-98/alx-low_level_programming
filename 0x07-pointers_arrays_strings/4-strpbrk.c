cef927ae2ab52d73ca9d823a6ebddd40e7b9a8bb
Ansah-98 <yungtupac99@gmail.com>
2023-07-31 06:54:17 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+/**
+ * _strpbrk - return no shit fuck this shit
+ *
+ * @s: receives a character:
+ *
+ * @accept: damn you just disturbing
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+char *_strpbrk(char *s, char *accept);
+{
+	return (strpbrk(s, accept));
+}
