868b409838225ad0765dcab7aa77e209196bb363
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 07:11:44 -0700

    updates

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _puts_recursion - return no shit fuck this shit
+ *
+ * @s: receives a character:
+ *
+ *
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <string.h>
+void _puts_recursion(char *s)
+{
+	puts(s)
+}
