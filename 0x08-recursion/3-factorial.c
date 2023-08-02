16578f406d2730115c3d340031ef00e507c44523
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 08:17:44 -0700

    udpates

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - prints string in reverse
+ * @n: string
+ *
+ * Return: nothing
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+	
+}
