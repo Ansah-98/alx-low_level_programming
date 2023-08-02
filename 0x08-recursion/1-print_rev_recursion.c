7bc9af7152146329f6c20cba8a033c44cf0f452a
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 08:00:02 -0700

    updates

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);											}
+	}
+}
