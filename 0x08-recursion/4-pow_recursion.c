2598918ad7edef166317f91e0cc1c58eec8cdc14
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 08:30:08 -0700

    updates

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints string in reverse
+ * @x: string
+ *
+ * @y: dont even care
+ *
+ * Return: nothing
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (_pow_recursion(x * _pow_recursion(x, y - 1));
+}
