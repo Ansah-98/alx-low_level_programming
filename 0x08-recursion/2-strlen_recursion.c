259b85eb47aad36a728ac20f019991b2ccbbb60d
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 08:11:04 -0700

    updates

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
