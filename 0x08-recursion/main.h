ac52a5cd44b818e907e7d1f53a183306c71a27ca
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 07:27:45 -0700

    updates

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void _puts_recursion(char *s);
+char *_memset(char *s, char b, unsigned int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+char *_strncat(char *dest, char *_src, int n);
+char* _strncpy(char* dest, const char* src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+int _putchar(char c);
+char *_strchr(char *s, char c);
+void set_string(char **s, char *to);
+#endif /* MAIN_H */
