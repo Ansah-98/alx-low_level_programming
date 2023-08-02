9d40b64f87f8d7677bdbb61f0e48b28bb38e0270
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 07:25:33 -0700

    updates

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,23 @@
 /**
  * _puts_recursion - return no shit fuck this shit
  *
- * @s: receives a character:
- *
- *
+ * @s: receives a character
  *
  * Description: check some stupid shit
  *
  * Return: 0 or 1 depending if upper or lower
  *
  */
+#include "main.h"
 #include <string.h>
 void _puts_recursion(char *s)
 {
-	puts(s);
+	int i;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		_putchar(s[i]);
+		if (s[i] == '\0')
+			_putchar('\n');
+	}
 }
