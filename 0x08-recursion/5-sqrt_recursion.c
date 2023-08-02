32a6c255930e6b08a933c4afc62fec5f0e407c2b
Ansah-98 <yungtupac99@gmail.com>
2023-08-02 08:49:43 -0700

    updates

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints string in reverse
+ * @x: string
+ *
+ * @y: dont even care
+ *
+ * Return: nothing
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (get_sqrt(n, 1));
+}
+/**
+ * get_sqrt - get square root of a number recursively
+ * @n: number
+ * @c: possible square root
+ * 
+ * Return: square root or -1
+ *
+ */
+int get_sqrt(int n, int c)
+{
+	if (c * c == n)
+		return (c);
+	else if (c > n / 2)
+		return (-1);
+	else
+		return (get_sqrt(n, ++c));	
+}
