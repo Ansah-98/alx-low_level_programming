7cc9c80a5e06516dde74e51f30d2f00fce487d93
Ansah-98 <yungtupac99@gmail.com>
2023-08-03 10:25:11 -0700

    updates

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+/**
+ * main - return no shit fuck this shit
+ *
+ * @argc: receives a character
+ *
+ * @argv: damn dont even care to care
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+int main(int  argc, char *argv[])
+{
+	int x, y, sum = 0;
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y]; y++)
+		{
+			if (argv[x][y] < '0' || argv[x][y] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
