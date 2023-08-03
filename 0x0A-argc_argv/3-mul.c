dc778e91ea9995e3f20ba6284ae5a95b30cf2123
Ansah-98 <yungtupac99@gmail.com>
2023-08-03 10:52:13 -0700

    updates

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,7 +20,7 @@ int main(int  argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n",atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
