14cdabd559f8d5c0224309d42ca968c384ac0018
Ansah-98 <yungtupac99@gmail.com>
2023-08-03 09:52:36 -0700

    updates

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+/**
+ * main - return no shit fuck this shit
+ *
+ * @argc: receives a character
+ *
+ * @argv: damn dont even care to care
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include "main.h"
+#include <string.h>
+int main(int  argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+
+	return (0)
+}
