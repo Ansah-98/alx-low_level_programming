c147a6c25940561b7161110e852495131d179021
Ansah-98 <yungtupac99@gmail.com>
2023-08-03 10:09:43 -0700

    updates

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+/**
+ * main - return no shit fuck this shit
+ *
+ * @argc: receives a character
+ *
+ * @argv: damn dont even care to care
+ *
+ * Description: check some stupid shit
+ *
+ * Return: 0 or 1 depending if upper or lower
+ *
+ */
+#include "main.h"
+#include <string.h>
+int main(int  argc, char __attributte__((unused)) *argv[])
+{
+	printf("%d\n",(int)argv[1] * (int)argv[2]);
+
+	return (0)
+}
