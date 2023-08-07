616cff8d12660450b61ac70fb1d25e765d85af35
Ansah-98 <yungtupac99@gmail.com>
2023-08-07 09:56:09 -0700

    updates

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+/**
+ * _strdup - creates an array
+ *
+ * @str :  size of array
+ *
+ * description: dont know a lot
+ *
+ * Return: returns an array or null
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+char *_strdup(char *str)
+{
+	int i;
+	char *a;
+
+	i = (int)strlen(str);
+
+	if (i == 0)
+		return (NULL);
+	a = malloc(sizeof(char) * (i + 1));
+	if (a == NULL)
+		return (NULL);
+
+	strcpy(a, str)
+
+	return (a);
+}
