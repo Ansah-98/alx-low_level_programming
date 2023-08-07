ca4743c37c5b789b1cbda55ec69a9e813a408137
Ansah-98 <yungtupac99@gmail.com>
2023-08-07 09:17:57 -0700

    updates

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+/**
+ * create_array - creates an array
+ *
+ * @size :  size of array
+ *
+ * @c : initializer character
+ *
+ * description: dont know a lot
+ *
+ * Return : returns an array or null
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	if (a == NULL)
+		Return NULL;
+
+	a = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		a[i] = c;
+	return (a);
+}
