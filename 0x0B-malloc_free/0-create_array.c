fba52547538caf8e64193f8b85c251869f5f3b5d
Ansah-98 <yungtupac99@gmail.com>
2023-08-07 09:32:01 -0700

    updates

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,7 +24,7 @@ char *create_array(unsigned int size, char c)
 	a = malloc(sizeof(char) * size);
 	if (a == NULL)
 		return (NULL);
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		a[i] = c;
 	return (a);
 }
