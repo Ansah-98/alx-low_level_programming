24b97de210c61729af0db2737be849fe78dc750e
Ansah-98 <yungtupac99@gmail.com>
2023-08-08 10:06:46 -0700

    updates

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,18 +10,19 @@
  * Return: returns a string pointer or null
  *
  */
+#include <string.h>
 #include <stdlib.h>
 char *argstostr(int ac, char **av)
 {
 	int i;
-	char **s;
+	char *s;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	s = malloc(((int)sizeof(char *) * (ac - 1) * 2) + 1);
 	for (i = 2; i < ac * 2; i += 2)
 	{
-		s[(i - 2) / 2] = av[i / 2];
+		strcat(s, av[i / 2]);
 		s[(i / 2) + 1] = "\n";
 	}
 	s[((ac - 1) * 2) + 1] = "\0";
