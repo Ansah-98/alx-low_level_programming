596a72eabfc588f98776445613a76927455c94d0
Ansah-98 <yungtupac99@gmail.com>
2023-08-08 10:59:17 -0700

    updates

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	i = (int)strlen(str);
-	if (str != "")
+	if (*str != '\0')
 		a = malloc(sizeof(char) * (i + 1));
 	else
 		a = malloc(1);
