9043cc55efc0fbd0089a9e241ebe4de03e8a28fa
Ansah-98 <yungtupac99@gmail.com>
2023-08-10 10:33:42 -0700

    updates

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+/**
+ * malloc_checked - returns a pointer
+ *
+ * @b:yh some number 
+ * 
+ * description: lets describe a pointer 
+ *
+ * Return: returns nothing special
+ *
+ */
+
+#include <stdlib.h>
+void *malloc_checked(unsigned int b)
+{
+	s = malloc(b)
+	return (s);
+}
