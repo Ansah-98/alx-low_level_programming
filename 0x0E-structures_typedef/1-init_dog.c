61dbd49118b69b035295a0a335c5b777979f1faf
Ansah-98 <yungtupac99@gmail.com>
2023-08-14 10:00:21 -0700

    updates

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
  * init_dog - naming a dog or something
  * @name: guess the  name again
  *
- * @age: you already know 
+ * @age: you already know
  * @owner: again be a good owner
  *
  * Return: we dont return anything doggo is poor
@@ -17,6 +17,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		(*d).name = name;
 		(*d).age = age;
 		(*d).owner = owner;
-	}		
+	}
 }
 #endif
