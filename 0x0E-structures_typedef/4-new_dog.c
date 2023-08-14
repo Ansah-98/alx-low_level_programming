ea81b40179a9e5911af80521760288af7c62670e
Ansah-98 <yungtupac99@gmail.com>
2023-08-14 15:21:30 -0700

    updates

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+#include <string>
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+	dog_t dog;
+
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return NULL;
+
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
+
+	dog->age = age;
+
+	return (dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,5 +15,6 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
