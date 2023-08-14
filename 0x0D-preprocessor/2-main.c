9b48fb078543420b494c1bc678f5e4fdba0a114c
Ansah-98 <yungtupac99@gmail.com>
2023-08-14 08:35:40 -0700

    updates

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - just does the main thing
+ *
+ * Return: nothing
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (EXIT_SUCCESS);
+}
