805a35b62ed3ef7e0eca03ce1c86a39cda06b259
Ansah-98 <yungtupac99@gmail.com>
2023-08-14 15:55:40 -0700

    updates

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,14 +5,15 @@
  *
  *
  */
-#include <string>
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	
-	dog_t dog;
+	dog_t *dog;
 
-	dog = (dog_t *)malloc(sizeof(dog_t));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return NULL;
 
