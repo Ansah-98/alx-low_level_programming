73f8f06eeac27e97b47a379efcda1de5307c118f
Ansah-98 <yungtupac99@gmail.com>
2023-08-14 17:37:50 -0700

    updates

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+/**
+ *
+ * free_dog - Frees dogs.
+ *
+ * @d: The dog to be freed.
+ *
+ *
+ *
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
