113523716b64b164a1df7d469c291b553929ab1f
Ansah-98 <yungtupac99@gmail.com>
2023-08-14 17:03:32 -0700

    updates

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	
 	dog_t *dog;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	dog = (dog_t *)malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return NULL;
-
+	dog->name = (char *)malloc(strlen(name) + 1);
+	dog->owner = (char *)malloc(strlen(owner) + 1);
+	if (dog->name == NULL || dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+		return (NULL);
+	}
 	strcpy(dog->name, name);
 	strcpy(dog->owner, owner);
 
