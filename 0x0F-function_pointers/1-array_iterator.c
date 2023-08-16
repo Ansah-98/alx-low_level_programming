9991f20826ed3640bf082c331cf893033ead8d44
Ansah-98 <yungtupac99@gmail.com>
2023-08-16 08:53:34 -0700

    updates

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,12 +8,11 @@
  * Return: return nothing
  *
  */
+#include <stdlib.h>
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-
 	if (!size || array || action)
 		return;
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	while (size--)
+		action(*array++);
 }
