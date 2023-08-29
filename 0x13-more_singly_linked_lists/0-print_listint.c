8f9cbcfcb0c4e088a7482bebe51c0d708067e969
Ansah-98 <yungtupac99@gmail.com>
2023-08-29 09:26:25 -0700

    updates

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_listint - does nothing special
+ * @h: pointer I guess 
+ *
+ * Return: returns counting
+ *
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t counting = 0;
