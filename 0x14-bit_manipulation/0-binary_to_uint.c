23234e5d96f39ec7761ebc491a00d65fbc8658a0
Ansah-98 <yungtupac99@gmail.com>
2023-08-31 05:41:47 -0700

    added 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,12 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (b[i] != '\0')
 	{
-		result = (b[i] == '0') ? (result << 1) : (result << 1) | 1;
-		if if (b[i] != '0' && b[i] != '1')
+		result = (b[i] == '0') ? (final << 1) : (final << 1) | 1;
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
-	return (result);
+	return (final);
 }
 
 
